756d770eb68dedf5a86a474b30c70db0fab9a948
Christopher Yu <cyu06@hotmail.com>
2015-09-10 21:11:45 -0700

    Deleted reliance on config.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var aws = require('./config').aws;
 var knox = require('knox');
 var knoxClient = knox.createClient({
   key: process.env.ACCESSKEYID,
